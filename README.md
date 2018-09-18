a2f25557b2afde85c0ead35be731891871547988
Javier Torres <javier.torresb@gmail.com>
2018-09-18 19:21:36 -0400

    Documentation improvements. .gitignore fixed.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ROS-Talon
-Ros package to interface with CTRE Talon SRX through CAN for steering control application.
+
+[ROS](http://www.ros.org/) package to interface with the Talon SRX motor controller from Cross The Road Electronics through CAN using the [socketcan_bridge](http://wiki.ros.org/socketcan_bridge) ROS package.
+
+# Usage and details
+
+## System configuration and hardware setup
+
+
+## Topics and services
+
+The package consists of two nodes: 
+
+## CAN frames
+
+# Known issues
+
+On first connection to the devices, it crashes at least three times.
+
+# Future development
--- talonsrx/src/servo_pos.cpp
+++ talonsrx/src/servo_pos.cpp
@@ -1,3 +1,9 @@
+/*
+Node to control a TalonSRX device on position servo mode.
+Internally, it subscribes to the /received_messages and /sent_messages topics of the type can_msgs::Frame from the socketcan_bridge package to talk to the Talon through CAN protocol. Keep in mind that to work properly it is required for your system to have a properly working CAN interface. A list of USB-CAN drivers/devices that are known to work with the socketcan_bridge package may ben fond at wiki.ros.org/socketcan_interface.
+This node also publishes the /talonsrx/current_position
+*/
+
 #include <talon_bridge/talon.h>
 
 int main(int argc, char **argv)
